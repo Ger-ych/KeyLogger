cde7778d0c4e5fc9159855a995fb46a9dc54f83d
ttqd <105600287+ttqd@users.noreply.github.com>
2022-10-28 19:01:39 +0500

    Improved Stability & Performance

--- KeyLogger/KeyLogger.py
+++ KeyLogger/KeyLogger.py
@@ -1,9 +1,13 @@
-# keylogger
-
 import pynput.keyboard
+import colorama
+
+colorama.init()
+print("Keylogger is working")
+
 
 def process_key_press(key):
-    print(key)
+    print(f"You are pressed button {colorama.Fore.RED}{key}{colorama.Fore.RESET}")
+
 
 KeyboardListener = pynput.keyboard.Listener(on_press=process_key_press)
 
